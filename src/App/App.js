7d60154ade9a871915c1be0cff92c6e98de474cc
Chad Schroeder <cschroed@adobe.com>
2020-09-11 11:59:39 -0700

    display suppression

--- src/App/App.js
+++ src/App/App.js
@@ -4,13 +4,21 @@ import { useSelector, useDispatch } from 'react-redux';
 import { Map } from '../Map';
 import { Units } from '../Units';
 
-const App = () => {
-    const { loading } = useSelector(state => state.common);
 
-    const dispatch = useDispatch();
+const App = () => {
+    const { loading, playerTurn, suppression } = useSelector(state => state.common);
 
     return (
         <>
+            <h1>Castle Itter</h1>
+            <h2>Stats</h2>
+            <p>Player turn: {playerTurn ? 'True' : 'False'}</p>
+            <p>Suppression tokens:</p>
+            <ul>
+                {Object.keys(suppression).map((key, i) => (
+                    <li key={key}>{key[0].toUpperCase() + key.slice(1)}: {suppression[key]}</li>
+                ))}
+            </ul>
             <Map />
             <Units />
         </>

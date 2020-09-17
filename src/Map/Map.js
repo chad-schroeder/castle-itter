961dbd452c7efdbfa31e5aa421c1d6a5218a1384
Chad Schroeder <cschroed@adobe.com>
2020-09-16 17:32:33 -0700

    removed commented out text

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,16 +1,3 @@
-/*
- * CASTLE ITTER
- *
- * License: MIT
- *
- * A non-commercial port of the solo boardgame "Castle Itter":
- * https://boardgamegeek.com/boardgame/196257/castle-itter
- * 
- * Designer: David Thompson
- * Artist: David Thompson, Matt W White
- * Publisher: Dan Verssen Games (DVG)
- */
-
 import React from 'react';
 import { useSelector } from 'react-redux';
 
--- src/Utils/Libs/getAvailableMoves.js
+++ src/Utils/Libs/getAvailableMoves.js
@@ -1,16 +1,3 @@
-/*
- * CASTLE ITTER
- *
- * License: MIT
- *
- * A non-commercial port of the solo boardgame "Castle Itter":
- * https://boardgamegeek.com/boardgame/196257/castle-itter
- * 
- * Designer: David Thompson
- * Artist: David Thompson, Matt W White
- * Publisher: Dan Verssen Games (DVG)
- */
-
 import store from '../../store';
 
 const getAvailableMoves = () => {

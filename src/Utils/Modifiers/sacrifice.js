a83c492babe6b1243520f646c76be87059dcefce
Chad Schroeder <cschroed@adobe.com>
2020-09-20 09:49:15 -0700

    added sacrifice

---
+++ src/Utils/Modifiers/sacrifice.js
@@ -0,0 +1,43 @@
+import store from '../../store';
+
+const tiles = store.getState().map.tiles;
+const friendlies = store.getState().units.friendly.units;
+
+const sacrifice = (unitId, tileId) => {
+    // get location of tile
+    const location = tiles.find(tile => tile.id === tileId).location;
+
+    // only one unit, 'gangl', can sacrifice, so check for him
+    const hasGangl = tiles.find(tile => tile.unit === 'gangl' && tile.location === location);
+
+    if (hasGangl) {
+        // gangl can be exhausted or disrupted and still sacrifice
+        const gangl = friendlies.find(unit => unit.id === 'gangl' && !unit.kia);
+
+        if (gangl) { 
+            // send user option to sacrifice
+            // await response
+                // for this example, always sacrifice
+
+            // update friendly units
+            const payload = friendlies.map(unit => {
+                if (unit.id === 'gangl') {
+                    return {
+                        ...unit,
+                        kia: true,
+                    };
+                }
+                return unit;
+            });
+            
+            // update friendly units
+            store.dispatch({ type: 'UPDATE_FRIENDLIES', payload });
+        }
+
+        return false;
+    }
+    
+    return false;
+};
+
+export default sacrifice;

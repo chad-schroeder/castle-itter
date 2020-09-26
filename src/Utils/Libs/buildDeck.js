30bcc4834644e068548415a14c7980aa199a8d08
Chad Schroeder <cschroed@adobe.com>
2020-09-25 17:08:12 -0700

    added shuffle, constructed deck

--- src/Utils/Libs/buildDeck.js
+++ src/Utils/Libs/buildDeck.js
@@ -2,33 +2,23 @@ import { v4 as uuidv4 } from 'uuid'; // uuidv4()
 
 import store from '../../store';
 
+const shuffle = (deck) => {
+    for (let i = deck.length - 1; i > 0; i -= 1) {
+      let randomIndex = Math.floor(Math.random() * (i + 1));
+      [deck[i], deck[randomIndex]] = [deck[randomIndex], deck[i]];
+    }
+    return deck;
+};
+
 const buildDeck = () => {
-    const enemies = store.getState().units.enemy.units;
-    const deck = [];
-
-    // individual decks
-    let d1 = [];
-    let d2 = [];
-    let d3 = [];
-    let d4 = [];
-
-    // riflemen
-    const riflemen = enemies.RF;
-
-    /*
-        RF: {
-            name: 'Rifleman',
-            type: 'rifleman',
-            defense: 3,
-            advance: true,
-            decks: {
-                1: [2, 2, 2],
-                2: [2, 2, 2],
-                3: [3],
-                4: [2, 2, 3, 3],
-            },
-        },
-    */
+    const decks = store.getState().common.decks;
+    
+    const deck = [
+        ...shuffle(decks[1]),
+        ...shuffle(decks[2]),
+        ...shuffle(decks[3]),
+        ...shuffle(decks[4]),
+    ];
 
     return deck;
 };

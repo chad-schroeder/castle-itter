b52e3617cfe6135a573a609d74ae98891bd29218
Chad Schroeder <cschroed@adobe.com>
2020-09-28 18:16:16 -0700

    updated sniper logic

--- src/Utils/Enemy/sniper.js
+++ src/Utils/Enemy/sniper.js
@@ -1,4 +1,4 @@
-import { rollDice, acquireTarget } from '../Libs/dice';
+import { getRandomIndex, rollDice, acquireTarget } from '../Libs/dice';
 
 import store from '../../store';
 
@@ -6,39 +6,37 @@ const sniper = () => {
     const { locations, tiles, targeting } = store.getState().map;
     const { SN: sniper } = store.getState().units.enemy;
 
-    // get target color
+    // determine target color
     const rollForColor = rollDice();
     const targetColor = targeting.SN[rollForColor];
 
-    // get target tiles
+    // get all tiles with target color
     const targetColors = targeting[targetColor];
-    console.log('targetColors', targetColors);
 
-    // roll for target tile
+    // search for a unit to target among possible tiles
     const targetUnit = acquireTarget(targetColors);
-    const targetTile = 'G2';
 
-    // check if target tile has a unit, otherwise search for next target
+    if (targetUnit) {
+        // if unit, find tile the unit occupies
+        const { location: locationId, id: tileId } = tiles.find(tile => tile.unit === targetUnit);
+
+        // if unit, get defense value of tile location
+        const { defense } = locations.find(location => location.id === locationId);
     
+        // // roll to hit
+        const attackValue = sniper.attack;
+        const targetRoll = rollDice(attackValue);
+    
+        if (targetRoll.some(dice => dice >= defense)) {
+            console.log('unit eliminated');
+        } else {
+            console.log('miss!');
+        }
 
-    // find location id from tile
-    const { location: locationId } = tiles.find(tile => tile.id === targetTile);
-    console.log('sniper location', locationId);
-
-    // if unit, get defense value of tile location
-    const { defense } = locations.find(location => location.id === locationId);
-    console.log('sniper defense', defense);
-
-    // roll to hit
-    const attackValue = sniper.attack;
-    const targetRoll = rollDice(attackValue);
-    console.log('dice result', targetRoll);
-
-    if (targetRoll.some(dice => dice >= defense)) {
-        console.log('unit eliminated');
-    } else {
-        console.log('miss!');
+        return;
     }
+    
+    console.log('sniper: no unit found');
 };
 
 export default sniper;

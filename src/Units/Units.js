c217780583b834a1b3b1b993c38b3d8488d2aeb3
Chad Schroeder <cschroed@adobe.com>
2020-10-02 17:05:02 -0700

    removed unneeded imports

--- src/Units/Units.js
+++ src/Units/Units.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const Units = () => {
-    const { allies, axis } = useSelector(state => state.units);
-
     return null;
 };
 

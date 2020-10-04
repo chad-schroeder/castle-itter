e2c70575a50941e985158578ad63c64d3d3dac2d
Chad Schroeder <cschroed@adobe.com>
2020-10-04 10:50:23 -0700

    added more logic to playCard

--- src/Utils/Libs/buildDeck.js
+++ src/Utils/Libs/buildDeck.js
@@ -1,3 +1,4 @@
+import { cardSniper } from 'Utils/Enemy/disruption';
 import store from '../../store';
 
 const shuffle = (deck) => {
@@ -10,13 +11,41 @@ const shuffle = (deck) => {
 
 export const playCard = () => {
     const { deck } = store.getState().common;
-    const topCard = deck[0];
 
-    console.log('play card', topCard);
+    if (deck.length) {
+        const { id = null, deck: deckId, card, target, type } = deck[0];
+        console.log('Play card:', type);
+        console.log('Current deck:', deckId);
 
-    // remove top card from deck
-    const cards = deck.filter(card => card.id !== topCard.id);
-    store.dispatch({ type: 'PLAYED_CARD', payload: cards });
+        switch(card) {
+            case 'advance': {
+                console.log('Action: Advance');
+                break;
+            }
+            case 'sniper': {
+                console.log('Action: Sniper');
+                break;
+            }
+            case 'suppress': {
+                console.log('Action: Suppress');
+                break;
+            }
+            case 'panzerfaust': {
+                console.log('Action: Panzerfaust');
+                break;
+            }
+            default: {
+                console.log('Action: Disrupt');
+                break;
+            }
+        }
+
+        // remove top card from deck, by id instead of .unshift() as a debug precaution
+        const cards = deck.filter(card => card.id !== id);
+        store.dispatch({ type: 'PLAYED_CARD', payload: cards });
+    }
+
+    console.log('Game Over: Deck exhausted');
 };
 
 export const buildDeck = () => {

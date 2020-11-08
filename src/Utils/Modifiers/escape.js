7cc92865973f7e9a95f52cc5a6c5bd4d5e7ea8f9
Chad Schroeder <cschroed@adobe.com>
2020-11-08 08:23:37 -0800

    added canEscape

--- src/Utils/Modifiers/escape.js
+++ src/Utils/Modifiers/escape.js
@@ -1,24 +1,42 @@
 import store from '../../store';
 
-const tiles = store.getState().map.tiles;
-const { allied } = store.getState().units;
+const { tracks } = store.getState().map;
+const { boratra } = store.getState().units.allies;
 
-export const canEscape = (tileId) => {
-    // borotra cannot escape from the Cellar of Great Hall
-    if (tileID !== 'C' || tileId !== 'GH') {
-        // determine which enemy entry tiles are vacant
-        // for this example, all are vacant
-        const vacancies = true;
+const getEmptyTrack = (color) => {
+    const enemies = [];
 
-        // get boratra
-        const boratra = { allied }.find(unit => unit.id === 'borotra');
-
-        if (!boratra.exhausted) {
-            
+    Object.keys(tracks).forEach(track => {
+        if (track.los === color && (track.counter || track.suppress.length)) {
+            enemies.push('enemy');
         }
+    });
 
-        return false;
+    return !enemies.length;
+};
+
+export const canEscape = (unitId, location, los) => {
+    if (unitId !== 'boratra') return;
+
+    // Borotra cannot escape from the Cellar of Great Hall
+    if (location === 'C' || location === 'GH') return;
+
+    // return if Boratra is not ready to escape
+    if (boratra.exhausted || boratra.tokens.hasAction || boratra.tokens.commanded || boratra.tokens.disrupted) return;
+
+    // check for empty track related to Boratra's present location
+    for (let color in los) {
+        const empty = getEmptyTrack(color);
+         
+        if (empty) {
+            return true;
+        }
     }
 
-    return false;
+    // Boratra cannot escape
+    return;
+};
+
+export const escape = (tileId) => {
+
 };

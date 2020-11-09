4ce10704963b95ec13a53f26a1fdaa14cae3dabe
Chad Schroeder <cschroed@adobe.com>
2020-11-08 21:11:21 -0800

    updated 'checkInspired'

--- src/Utils/Modifiers/inspire.js
+++ src/Utils/Modifiers/inspire.js
@@ -1,31 +1,32 @@
+import { getLocationUnits } from 'Utils/Libs/units';
+
 import store from '../../store';
 
-const { tiles } = store.getState().map;
 const { allies } = store.getState().units;
 
-const inspire = (tileId, location, unitId) => {
+const checkInspired = (locationId, unitId) => {
     // if location is the cellar, return
-    if (location === 'C') return;
+    if (locationId === 'C') return;
     
-    // find all friendlies at location
-    const units = tiles
-        .filter(tile => 
-            tile.unit && 
-            tile.location === location && 
-            tile.unit !== unitId)
-        .map(tile => tile.unit);
+    // find other units at location
+    const units = getLocationUnits(locationId, unitId);
 
     // iterate through units at location
-    for (let unit in units) {
+    const inspired = units.some(unit => {
         const ally = allies[unit];
 
-        // if unit with inspire is at location and able to inspire, return true
-        if (ally.inspire && !ally.exhausted && !ally.tokens.includes('disrupted')) {
+        if (ally.inspire && !ally.exhaused && !ally.tokens.includes('disrupted')) {
             return true;
         }
+
+        return false;
+    });
+
+    if (inspired) {
+        return true;
     }
 
     return false;
 };
 
-export default inspire;
+export default checkInspired;

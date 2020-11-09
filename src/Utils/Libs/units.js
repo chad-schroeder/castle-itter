f1a43ef6de4b80d707352263867f77fcafdfcc6b
Chad Schroeder <cschroed@adobe.com>
2020-11-08 21:13:30 -0800

    updated 'getUnit'

--- src/Utils/Libs/units.js
+++ src/Utils/Libs/units.js
@@ -3,15 +3,40 @@ import store from '../../store';
 const { tiles } = store.getState().map;
 const { allies } = store.getState().units;
 
-export const getLocationUnits = locationId => {
+// get unit stats
+export const getUnit = unitId => allies[unitId];
+
+// verify unit can take an action
+export const canTakeAction = unitId => {
+    const unit = allies[unitId];
+
+    if (unit.exhausted || unit.tokens.length) {
+        return false;
+    }
+
+    return true;
+};
+
+export const getLocationUnits = (locationId, skipId = null) => {
+    console.log('getLocationUnits', { locationId, skipId });
+
     const units = tiles
-        .filter(tile => tile.location === locationId && tile.unit)
+        .filter(tile => 
+            tile.unit &&
+            tile.unit !== skipId &&
+            tile.location === locationId)
         .map(tile => tile.unit);
     return units;
 };
 
-export const getActionableUnits = (list, skipId) => {
-    
+export const getActionableUnits = (list, skipId = null) => {
+    const units = list
+        .filter(unit => 
+            !unit.exhausted && 
+            !unit.tokens.length && 
+            !unit.tokens.length && unit.id !== skipId
+        );
+    return units;
 };
 
 export const eliminateUnits = units => {

662c50ae4f4438534b198d3424b1da895549c21d
Chad Schroeder <cschroed@adobe.com>
2020-11-10 21:03:33 -0800

    added unit functions

--- src/Utils/Libs/units.js
+++ src/Utils/Libs/units.js
@@ -39,11 +39,63 @@ export const getActionableUnits = (list, skipId = null) => {
     return units;
 };
 
-export const eliminateUnits = units => {
-   
-};
-
 export const getTargetsByColor = color => {
     const colors = store.getState().map.colors;
     return colors[color];
+};
+
+export const exhaustUnit = unitId => {
+    const payloadUnits = allies.map(unit => {
+        if (unit.id === unitId) {
+            return { ...unit, exhausted: true, tokens: ['action'] };
+        }
+        return unit;
+    });
+
+    store.dispatch({ type: 'UPDATE_ALLIES', payload: payloadUnits });
+};
+
+export const recoverExhausted = unitId => {
+    const payloadUnits = allies.map(unit => {
+        if (unit.id === unitId) {
+            return { ...unit, exhausted: false };
+        }
+        return unit;
+    });
+
+    store.dispatch({ type: 'UPDATE_ALLIES', payload: payloadUnits });
+};
+
+export const recoverToken = (unitId, removeToken) => {
+    const payloadUnits = allies.map(unit => {
+        if (unit.id === unitId) {
+            const tokens = unit.tokens.filter(token => token !== removeToken);
+            return { ...unit, tokens };
+        }
+        return unit;
+    });
+
+    store.dispatch({ type: 'UPDATE_ALLIES', payload: payloadUnits });
+};
+
+export const tallyCasualty = (unitId, tileId) => {
+    // remove unit from tile
+    const payloadTiles = tiles.map(tile => {
+        if (tile.id === tileId) {
+            return { ...tile, unit: null };
+        }
+        return tile;
+    });
+
+    store.dispatch({ type: 'UPDATE_TILES', payload: payloadTiles });
+
+    // update unit to casualty
+    const payloadUnits = allies.map(unit => {
+        if (unit.id === unitId) {
+            return { ...unit, casualty: true };
+        }
+        return unit;
+    });
+
+    store.dispatch({ type: 'UPDATE_ALLIES', payload: payloadUnits });
 };

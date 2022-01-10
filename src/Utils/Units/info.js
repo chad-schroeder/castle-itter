48885e0ac335bed0f6c754b8e34531da339e2bd3
Chad Schroeder <cschroed@adobe.com>
2022-01-09 17:57:36 -0800

    added info text

---
+++ src/Utils/Units/info.js
@@ -0,0 +1,9 @@
+export const info = {
+    phase_deployment_title: 'Deployment Phase',
+    phase_deployment_place: 'Only units from the Deployment pool may be placed onto the map. Once placed, that unit may take an action.',
+    phase_deployment_condition: 'Placed units may not be activated again until all units in the Deployment pool have been placed',
+    phase_cellar_title: 'Cellar Unlocked!',
+    phase_cellar_move: 'Cellar units may only move to the Great Hall. Units in the Great Hall may move to the Cellar.',
+    phase_reinforcement_title: 'Reinforcements Arrive!',
+    phase_reinforccement_place: 'Reinforcement units may be placed on any vacant tile. Once placed, the Reinforcement unit may take an action',
+};
